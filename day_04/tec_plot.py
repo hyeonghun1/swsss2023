f68ba055e632272399f890230426a62ccd878872
Hyeonghun Kim <hyk049@ucsd.edu>
2023-07-27 20:33:02 -0400

    tec_plot

---
+++ day_04/tec_plot.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 27 15:35:14 2023
+
+@author: hyeonghun
+"""
+
+from wam_ipe_plotter import plot_var, save2png
+import sys 
+import netCDF4 as nc
+
+if __name__ == '__main__':
+    
+    filename = sys.argv[1]
+    
+    # '/Users/hyeonghun/Desktop/wfs.t06z.20230726_10/wfs.t06z.ipe05.20230726_105500.nc'
+    
+    var_name = 'HmF2'
+    
+    infilename = filename
+    
+    png = save2png(infilename, var_name)
+    
+    
+
+# python_tec_plot.py wfs.t06z.ipe05.20230726_105500.nc
+
+# sys.argv[1]
+
+
