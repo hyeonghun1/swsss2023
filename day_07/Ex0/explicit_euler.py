d4c91dd7df5e74a4812a0ba006beab7cade26d99
Hyeonghun Kim <hyk049@ucsd.edu>
2023-08-01 13:21:18 -0400

    explicit Euler

--- day_07/Ex0/explicit_euler.py
+++ day_07/Ex0/explicit_euler.py
@@ -15,3 +15,42 @@ import matplotlib.pyplot as plt
 # Compare the numerical approximation of the IVP solution to its analytical
 # solution by plotting both solutions in the same figure. 
 
+
+x_0 = 3     # I.C
+
+h = 0.01    # time step
+
+Tfinal = 2
+
+time = np.linspace(0,2,201)
+
+x_current = x_0
+
+x = np.zeros(len(time))
+
+x[0] = x_0
+
+for ii in range(len(time)-1):
+    x_next = x_current + h * (-2*x_current)
+    
+    x[ii+1] = x_next
+    
+    x_current = x_next
+    
+    
+    
+    
+#%% Analytical soln
+    
+x_analytical = 3*np.exp(-2*time)
+
+
+#%% plot
+plt.plot(time, x)
+plt.plot(time, x_analytical, color = 'green',linestyle = 'dashed', linewidth = 2.0)
+plt.xlabel('Time')
+plt.ylabel('x(t)')
+plt.title('Time evolution of x(t)')
+plt.legend(['numerical', 'analytical'])
+
+
